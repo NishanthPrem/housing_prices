f3c04764c5f69f26f110836069d03ce92b1a4056
Nishanth Prem <nishanthprem8@gmail.com>
2024-06-19 14:55:50 -0700

    Updated code for final submission 2.

--- house_price_prediction.py
+++ house_price_prediction.py
@@ -3,38 +3,44 @@
 import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 
 #%% Loading the train set
 
 df_train = pd.read_csv('train.csv')
-benchmark_features = ['YearBuilt', 'MoSold', 'LotArea', 'BedroomAbvGr']
-
-X_train = df_train[benchmark_features]
+X_train = df_train.drop(columns=['SalePrice'])
 y_train = df_train['SalePrice']
-
 
 #%% Loading the Test set
 
 df_test = pd.read_csv('test.csv')
-X_test = df_test[benchmark_features]
+X_test= df_test.copy()
 
+#%% Preprocessing
+
+numerical_cols = X_train.select_dtypes(include=['int64', 'float64']).columns.tolist()
+categorical_cols = X_train.select_dtypes(include=['object']).columns.tolist()
 
 #%% Handle missing data
 
 num_imputer = SimpleImputer(strategy='median')
-X_train = num_imputer.fit_transform(X_train)
-X_test = num_imputer.transform(X_test)
+X_train[numerical_cols] = num_imputer.fit_transform(X_train[numerical_cols])
+X_test[numerical_cols] = num_imputer.transform(X_test[numerical_cols])
 
+cat_imputer = SimpleImputer(strategy='most_frequent')
+X_train[categorical_cols] = cat_imputer.fit_transform(X_train[categorical_cols])
+X_test[categorical_cols] = cat_imputer.transform(X_test[categorical_cols])
 
 #%% Feature scaling
 
 preprocessor = ColumnTransformer(
     transformers=[
-        ('num', StandardScaler(), slice(0, len(benchmark_features))),
+        ('num', StandardScaler(), numerical_cols),
+        ('cat', OneHotEncoder(handle_unknown='ignore'), categorical_cols) 
     ],
-    remainder='passthrough' )
+    remainder='passthrough'  
+)
 
 X_train = preprocessor.fit_transform(X_train)
 X_test = preprocessor.transform(X_test)
@@ -46,8 +52,7 @@ regressor.fit(X_train, y_train)
 
 y_pred = regressor.predict(X_test)
 
-#%% Submission File
-
+#%% Generating the submission file
 submission = pd.DataFrame({
     'Id': df_test['Id'],
     'SalePrice': y_pred
