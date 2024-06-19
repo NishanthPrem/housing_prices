6926f3dff5c5e63beb513db23e6b0523588657dc
Nishanth Prem <nishanthprem8@gmail.com>
2024-06-19 00:27:59 -0700

    Updated code for final submission.

--- house_price_prediction.py
+++ house_price_prediction.py
@@ -1,50 +1,40 @@
 #%% Importing the Libraries
 
 import pandas as pd
-import numpy as np
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import cross_val_score
 
 #%% Loading the train set
 
 df_train = pd.read_csv('train.csv')
-X_train = df_train.drop(columns=['SalePrice'])
+benchmark_features = ['YearBuilt', 'MoSold', 'LotArea', 'BedroomAbvGr']
+
+X_train = df_train[benchmark_features]
 y_train = df_train['SalePrice']
+
 
 #%% Loading the Test set
 
 df_test = pd.read_csv('test.csv')
-X_test= df_train.drop(columns=['SalePrice'])
-y_test = df_train['SalePrice']
+X_test = df_test[benchmark_features]
 
-#%% Preprocessing
-
-numerical_cols = X_train.select_dtypes(include=['int64', 'float64']).columns.tolist()
-categorical_cols = X_train.select_dtypes(include=['object']).columns.tolist()
 
 #%% Handle missing data
 
 num_imputer = SimpleImputer(strategy='median')
-X_train[numerical_cols] = num_imputer.fit_transform(X_train[numerical_cols])
-X_test[numerical_cols] = num_imputer.transform(X_test[numerical_cols])
+X_train = num_imputer.fit_transform(X_train)
+X_test = num_imputer.transform(X_test)
 
-cat_imputer = SimpleImputer(strategy='most_frequent')
-X_train[categorical_cols] = cat_imputer.fit_transform(X_train[categorical_cols])
-X_test[categorical_cols] = cat_imputer.transform(X_test[categorical_cols])
 
 #%% Feature scaling
 
 preprocessor = ColumnTransformer(
     transformers=[
-        ('num', StandardScaler(), numerical_cols),
-        ('cat', OneHotEncoder(handle_unknown='ignore'), categorical_cols) 
+        ('num', StandardScaler(), slice(0, len(benchmark_features))),
     ],
-    remainder='passthrough'  
-)
+    remainder='passthrough' )
 
 X_train = preprocessor.fit_transform(X_train)
 X_test = preprocessor.transform(X_test)
@@ -56,12 +46,11 @@ regressor.fit(X_train, y_train)
 
 y_pred = regressor.predict(X_test)
 
+#%% Submission File
 
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
+submission = pd.DataFrame({
+    'Id': df_test['Id'],
+    'SalePrice': y_pred
+})
 
-#%% Cross Validation
-
-cv_scores = cross_val_score(regressor, X_train, y_train, cv=5, scoring='r2')
-print("Cross-Validation R² Scores:", cv_scores)
-print("Mean CV R² Score:", np.mean(cv_scores))
+submission.to_csv('submission.csv', index=False)
